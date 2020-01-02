f61c686e5b5ce2242143393b90ba9de5d7d033c6
Wesley Moses <wesmoses@gmail.com>
2020-01-02 16:37:40 +0200

    Updates

---
+++ precache-manifest.63b23edc55e25e8a22f0d27f8f6d02a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036437038ee7a79f3352c1c0d25ed049",
+    "url": "/wes337/astar-search-visualization/index.html"
+  },
+  {
+    "revision": "4b24eb49a518cc0011b8",
+    "url": "/wes337/astar-search-visualization/static/css/main.2722f420.chunk.css"
+  },
+  {
+    "revision": "19d26ba3e0c7fe6fc98c",
+    "url": "/wes337/astar-search-visualization/static/js/2.b085faaf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wes337/astar-search-visualization/static/js/2.b085faaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b24eb49a518cc0011b8",
+    "url": "/wes337/astar-search-visualization/static/js/main.034f5361.chunk.js"
+  },
+  {
+    "revision": "c7ab2853429fd3af36a1",
+    "url": "/wes337/astar-search-visualization/static/js/runtime-main.dabee7f6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wes337/astar-search-visualization/precache-manifest.63b23edc55e25e8a22f0d27f8f6d02a1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wes337/astar-search-visualization/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
